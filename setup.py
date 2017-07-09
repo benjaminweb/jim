cd2cb0538fe412b9594ddc9c26ead4ebbe6a47e6
Benjamin Weber <one@bwe.im>
2017-07-09 21:40:42 +0200

    Add long_description to setup.py: To show README on pypi.

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
 
 setup(
     name='jim',
     packages=['jim'],
     version='0.0.1',
     description='library to retrieve data from German railways',
+    long_description=long_description,
     url='https://bitbucket.org/hyllos/jim',
     author='Benjamin Weber',
     author_email='mail@bwe.im',
