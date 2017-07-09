518dbb6267016946ce985ecfe1f0a1fe341c905b
Benjamin Weber <one@bwe.im>
2017-07-09 21:54:55 +0200

    Bump version to 0.0.2 to get README on pypi.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='jim',
     packages=['jim'],
-    version='0.0.1',
+    version='0.0.2',
     description='library to retrieve data from German railways',
     long_description=long_description,
     url='https://bitbucket.org/hyllos/jim',
